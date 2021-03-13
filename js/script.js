fc356c7e8163eb6027abba9919932ff7fae52ce8
WJBFks <624662062@qq.com>
2021-03-13 16:48:49 +0800

    增加了三个选择滑块

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
 let times = 0
+let maxPoint = 6
+let refreshRate = 10
+let refreshNumber = 10
+
+function change() {
+	maxPoint = $("#MaxPointInput")[0].value
+	$("#MaxPointLabel").html(maxPoint)
+	refreshRate = $("#RefreshRateInput")[0].value
+	$("#RefreshRateLabel").html(refreshRate)
+	refreshNumber = $("#RefreshNumberInput")[0].value
+	$("#RefreshNumberLabel").html(refreshNumber)
+}
+
 
 function start() {
-	times = 10
+	times = refreshNumber
 	setTimeout("run()", 0)
 }
 
 function run() {
 	if (times > 0) {
-		let num = randomNum(1, 6)
+		let num = randomNum(1, maxPoint)
 		$("#image").attr("src", "./img/" + num + ".png")
 		times--;
-		setTimeout("run()", 100)
+		setTimeout("run()", 1000 / refreshRate)
 	}
 }
 
